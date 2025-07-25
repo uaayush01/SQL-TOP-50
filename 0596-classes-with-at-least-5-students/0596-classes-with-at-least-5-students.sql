e5f23b4959b07d3ca64f8b112c8c4075b7687273
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2025-07-25 11:35:00 +0530

    Time: 405 ms (11.49%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,2 +1,4 @@
-# Write your MySQL query statement below
- select  a.class from Courses a left join Courses b on a.class = b.class group by a.class having count(b.class) / 2 >= 5;
+select class
+from courses
+group by class
+having count(student)>=5;
