14a35f90fa186fee7dd2ef7f1321b23a0c54e1eb
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2025-07-31 14:52:23 +0530

    Time: 416 ms (59.31%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select max(num) as num 
 from (
-    select num from MyNumbers group by num having count(num) = 1
+    select num from MyNumbers group by num having count(*) = 1
 ) as unique_numbers;
